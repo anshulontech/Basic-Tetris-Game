5774be187d2636a728be0f1d47bea8093f0c33cc
anshul7sh <anshul7sh@gmail.com>
2020-05-21 19:12:00 +0530

    Project For Basic Tetris Game Initialized

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+
+    const width = 10
+
+
+
+})
