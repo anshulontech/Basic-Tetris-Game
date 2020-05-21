d9ba02fb27f61fffc84c759a14ed0f4e3f12679d
anshul7sh <anshul7sh@gmail.com>
2020-05-22 00:07:48 +0530

    Project Modified

--- app.js
+++ app.js
@@ -6,6 +6,136 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const width = 10
 
+    // The Tetrominoes
 
+    const lTetromino = [
+        [1, width + 1, 2 * width + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, 2 * width + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
 
+    const zTetromino = [
+        [0, width, width + 1, 2 * width + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1]
+    ]
+
+    const tTetromino = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1]
+    ]
+
+    const iTetromino = [
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3]
+    ]
+
+    const theTerominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+    let currentPosition = 4
+    let currentRotation = 0
+
+    let random = Math.floor(Math.random() * theTerominoes.length)
+    console.log(random)
+    let current = theTerominoes[random][currentRotation]
+
+    // draw the tetromino 
+
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+        })
+    }
+
+    // undraw the tetromino
+
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+    // make down the tetromino move down every second
+    timerId = setInterval(moveDown, 1000)
+
+    // assign function to keycodes
+    function control(e) {
+        if (e.keyCode === 37) {
+            moveleft()
+        } else if (e.keyCode == 38) {
+            // rotate()
+        } else if (e.keyCode == 39) {
+            moveRight()
+        } else if (e.keyCode == 40) {
+            moveDown()
+        }
+    }
+
+    document.addEventListener('keyup', control)
+
+    // move down function
+
+    function moveDown() {
+        undraw()
+        currentPosition += width
+        draw()
+        freeze()
+    }
+
+    // freeze function
+
+    function freeze() {
+        if (current.some(index => squares[currentPosition + width + index].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            // start the new tetromino falling 
+
+            random = Math.floor(Math.random() * theTerominoes.length)
+            current = theTerominoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+    }
+
+    // move the tetromino left unless is at the edge or blockage
+
+    function moveleft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if (!isAtLeftEdge) currentPosition -= 1
+
+        if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition += 1
+        }
+
+        draw()
+    }
+
+    // move the tetromino right unless is at the edge or blockage
+
+    function moveRight() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+
+        if (!isAtRightEdge) currentPosition += 1
+
+        if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -= 1
+        }
+
+        draw()
+    }
 })
